773727406497e061a22375ad2926e17639afc7fa
kr1vmehta <kr1vmehta@gmail.com>
2026-03-21 15:52:01 +0530

    Time: 6 ms (46.54%), Space: 20.7 MB (19.09%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,4 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        j=0
+        n=len(nums)
+        c=0
+        for i in range(n):
+            if nums[i]!=0:
+                nums[j]=nums[i]
+                j+=1
+            else:
+                c+=1
+        for i in range(n-c,n):
+            nums[i]=0
         
